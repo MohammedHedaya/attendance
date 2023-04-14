37cc029220cee988ec2049ed72650bf3a32b7791
MohammedHedaya <110480122+MohammedHedaya@users.noreply.github.com>
2023-04-14 04:48:28 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:attendance/add.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'mainpage.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // if (kIsWeb) {
+  //   await Firebase.initializeApp(
+  //     options: const FirebaseOptions(
+  //       apiKey: "",
+  //       authDomain: "",
+  //       projectId: "",
+  //       storageBucket: "",
+  //       messagingSenderId: "",
+  //       appId: "",
+  //       measurementId: "",
+  //     ),
+  //   );
+  // } else {
+  await Firebase.initializeApp();
+  // }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Attendance app',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Attendance app'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              // height: 50,
+              // width: w,
+              child: MaterialButton(
+                color: const Color.fromARGB(255, 2, 0, 133),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => mainpage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Attendance',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              // height: 50,
+              // width: w,
+              child: MaterialButton(
+                color: const Color.fromARGB(255, 2, 0, 133),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => add(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'ADD',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
